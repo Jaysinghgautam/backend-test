6175c977376d5ca44050b6642eaf2cd928a95606
Jaysinghgautam <gjaysingh53@gmail.com>
2026-03-26 11:08:18 +0530

    dfad

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ app.get('/', (req, res) => {
 app.get('/login',(req,res) => {
     res.send("hello from login")
 })
+
+app.get("/ping", (req, res) => {
+  res.status(200).json({
+    status: "ok",
+    message: "Server is running 🚀",
+    time: new Date()
+  });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
